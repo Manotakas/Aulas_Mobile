08945fb5f50a86273ab3a2f795263f023245d3ee
Manotakas <takedi006@gmail.com>
2024-11-13 14:35:21 -0300

    sla

---
+++ Atividade login/login/src/App.jsx
@@ -0,0 +1,20 @@
+import './script.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Formulario cadrastro</h1>
+        <br/>
+      <p>Nome:</p> 
+      <input type="text" id='nome'/>
+      <p>email:</p> 
+      <input type="text" id='nome'/>
+      <p>Data de Nascimento:</p> 
+      <input type="text" id='nome'/>
+
+    </>
+  )
+}
+
+export default App
